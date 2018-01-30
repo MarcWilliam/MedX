e112da8fd8533fea528bf7aa07ef72df9520a4e7
AllaDiaa <AllaDiaa@users.noreply.github.com>
2018-01-30 03:49:53 +0200

    no message

--- MedX-developer/src/pages/home/home.ts
+++ MedX-developer/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import { StatisticsPage } from '../statistics/statistics';
 import { HistoryPage } from '../history/history';
@@ -10,42 +10,63 @@ import { Query } from './query';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
-  private query:string;
-  private queryCost:number;
-  private submit:any;
 
-  public queries:Query[] = new Array();
-  private statisticsPage:any;
-  private historyPage:any;
+  private query: string;
+  private queryCost: number;
+  private submit: any;
 
-  constructor(public navCtrl: NavController) {
+  public queries: Query[] = new Array();
+  private statisticsPage: any;
+  private historyPage: any;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     this.statisticsPage = StatisticsPage;
     this.historyPage = HistoryPage;
   }
 
-  logForm(){
+  logForm() {
 
     HistoryPage.queries.push(new Query(this.query, this.queryCost));
     this.navCtrl.push(this.statisticsPage);
 
     this.query = '';
 
-    if(!this.submit.hidden)
-      this.submit.hidden = true;
 
   }
 
-  calcCost(){
-    if(this.query.length > 0 && this.query.length < 20){
+
+  showConfirm() {
+
+    if (this.query.length <= 0) {
+      return;
+    }
+    else if (this.query.length < 20) {
       this.queryCost = 20;
-    }else if(this.query.length > 20){
+    } else if (this.query.length > 20) {
       this.queryCost = 50;
     }
 
-    this.submit = document.getElementById('submit');
-    
-    this.submit.hidden = false;
+    let confirm = this.alertCtrl.create({
+      title: `Are you sure you want to execute this query?`,
+      message: `This service will cost you ${this.queryCost} LE`,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('OK clicked');
+
+            this.logForm();
+          }
+        },
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
