48b9355296c9a1c0765def795f9e3ff1d08a21ca
AllaDiaa <AllaDiaa@github.com>
2018-02-01 23:10:33 +0200

    no message

--- MedX-patient/src/app/app.component.ts
+++ MedX-patient/src/app/app.component.ts
@@ -5,32 +5,25 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { Storage } from '@ionic/storage';
 
-import { AccountPage } from '../pages/account/account';
-import { ConferenceData } from '../providers/conference-data';
 import { DemographicsPage } from '../pages/demographics/demographics';
 import { LaboratoryPage } from '../pages/laboratory/laboratory';
-import { LoginPage } from '../pages/login/login';
 import { OfficeVisitsPage } from '../pages/office-visits/office-visits';
-import { SignupPage } from '../pages/signup/signup';
 import { SummaryPage } from '../pages/summary/summary';
-import { SupportPage } from '../pages/support/support';
 import { SurgeriesPage } from '../pages/surgeries/surgeries';
-import { TabsPage } from '../pages/tabs-page/tabs-page';
-import { UserData } from '../providers/user-data';
+import { ProvidersPage } from '../pages/providers/providers';
 
 export interface PageInterface {
   title: string;
   name: string;
   component: any;
   icon: string;
-  logsOut?: boolean;
   index?: number;
   tabName?: string;
   tabComponent?: any;
 }
 
 @Component({
-  templateUrl: 'app.template.html'
+  templateUrl: './app.template.html'
 })
 export class ConferenceApp {
   // the root nav is a child of the root app component
@@ -48,64 +41,24 @@ export class ConferenceApp {
     { title: 'Surgeries', name: 'SurgeriesPage', component: SurgeriesPage, tabComponent: SurgeriesPage, index: 3, icon: 'information-circle' }
 
   ];
-  loggedInPages: PageInterface[] = [
-    { title: 'Account', name: 'AccountPage', component: AccountPage, icon: 'person' },
-    { title: 'Support', name: 'SupportPage', component: SupportPage, icon: 'help' },
-    { title: 'Logout', name: 'TabsPage', component: TabsPage, icon: 'log-out', logsOut: true }
-  ];
-  loggedOutPages: PageInterface[] = [
-    { title: 'Login', name: 'LoginPage', component: LoginPage, icon: 'log-in' },
-    { title: 'Support', name: 'SupportPage', component: SupportPage, icon: 'help' },
-    { title: 'Signup', name: 'SignupPage', component: SignupPage, icon: 'person-add' }
+  navPages: PageInterface[] = [
+    { title: 'Providers', name: 'ProvidersPage', component: ProvidersPage, tabComponent: ProvidersPage, index: 0, icon: 'person' }
+
   ];
   rootPage: any;
 
   constructor(
     public events: Events,
-    public userData: UserData,
     public menu: MenuController,
     public platform: Platform,
-    public confData: ConferenceData,
     public storage: Storage,
     public splashScreen: SplashScreen
   ) {
-/*
-    // load the conference data
-    confData.load();
-
-    // decide which menu items should be hidden by current login status stored in local storage
-    this.userData.hasLoggedIn().then((hasLoggedIn) => {
-      this.enableMenu(hasLoggedIn === true);
-    });
-    this.enableMenu(true);
-
-    this.listenToLoginEvents();
-  */this.rootPage = DemographicsPage;
+    
+    this.rootPage = DemographicsPage;
   }
 
   openPage(page: PageInterface) {
-    let params = {};
-
-    /*
-    // the nav component was found using @ViewChild(Nav)
-    // setRoot on the nav to remove previous pages and only have this page
-    // we wouldn't want the back button to show in this scenario
-    if (page.index) {
-      params = { tabIndex: page.index };
-    }
-
-    // If we are already on tabs just change the selected tab
-    // don't setRoot again, this maintains the history stack of the
-    // tabs even if changing them from the menu
-    if (this.nav.getActiveChildNavs().length && page.index != undefined) {
-      this.nav.getActiveChildNavs()[0].select(page.index);
-    } else {
-      // Set the root of the nav with params if it's a tab index
-      this.nav.setRoot(page.name, params).catch((err: any) => {
-        console.log(`Didn't set nav root: ${err}`);
-      });
-    }
-*/
     if (page.index) {
       this.nav.setRoot(page.component, { tabIndex: page.index });
     } else {
@@ -114,30 +67,6 @@ export class ConferenceApp {
       });
     }
 
-    if (page.logsOut === true) {
-      // Give the menu time to close before changing to logged out
-      this.userData.logout();
-    }
-  }
-
-
-  listenToLoginEvents() {
-    this.events.subscribe('user:login', () => {
-      this.enableMenu(true);
-    });
-
-    this.events.subscribe('user:signup', () => {
-      this.enableMenu(true);
-    });
-
-    this.events.subscribe('user:logout', () => {
-      this.enableMenu(false);
-    });
-  }
-
-  enableMenu(loggedIn: boolean) {
-    this.menu.enable(loggedIn, 'loggedInMenu');
-    this.menu.enable(!loggedIn, 'loggedOutMenu');
   }
 
   platformReady() {
--- MedX-patient/src/app/app.module.ts
+++ MedX-patient/src/app/app.module.ts
@@ -11,74 +11,43 @@ import { IonicStorageModule } from '@ionic/storage';
 
 import { ConferenceApp } from './app.component';
 
-import { PopoverPage } from '../pages/about-popover/about-popover';
-import { AccountPage } from '../pages/account/account';
-import { LoginPage } from '../pages/login/login';
-import { SessionDetailPage } from '../pages/session-detail/session-detail';
-import { SignupPage } from '../pages/signup/signup';
-import { SupportPage } from '../pages/support/support';
-import { TabsPage } from '../../.tmp/src/pages/tabs-page/tabs-page';
-
-import { ConferenceData } from '../providers/conference-data';
-import { UserData } from '../providers/user-data';
-
 import { DemographicsPage } from '../pages/demographics/demographics';
 import { LaboratoryPage } from '../pages/laboratory/laboratory';
 import { OfficeVisitsPage } from '../pages/office-visits/office-visits';
 import { SummaryPage } from '../pages/summary/summary';
 import { SurgeriesPage } from '../pages/surgeries/surgeries';
+import { ProvidersPage } from '../pages/providers/providers';
 
 @NgModule({
   declarations: [
     ConferenceApp,
-    AccountPage,
-    LoginPage,
-    PopoverPage,
-    SessionDetailPage,
-    SignupPage,
-    SupportPage,
     DemographicsPage,
     LaboratoryPage,
     OfficeVisitsPage,
     SummaryPage,
     SurgeriesPage,
-    TabsPage
+    ProvidersPage
   ],
   imports: [
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(ConferenceApp, {}, {
-      links: [
-        { component: TabsPage, name: 'TabsPage', segment: 'tabs' },
-        { component: SessionDetailPage, name: 'SessionDetail', segment: 'sessionDetail/:sessionId' },
-        { component: SupportPage, name: 'SupportPage', segment: 'support' },
-        { component: LoginPage, name: 'LoginPage', segment: 'login' },
-        { component: AccountPage, name: 'AccountPage', segment: 'account' },
-        { component: SignupPage, name: 'SignupPage', segment: 'signup' }
-      ]
+      links: []
     }),
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     ConferenceApp,
-    AccountPage,
-    LoginPage,
-    PopoverPage,
-    SessionDetailPage,
-    SignupPage,
-    SupportPage,
     DemographicsPage,
     LaboratoryPage,
     OfficeVisitsPage,
     SummaryPage,
     SurgeriesPage,
-    TabsPage
+    ProvidersPage
   ],
   providers: [
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    ConferenceData,
-    UserData,
     InAppBrowser,
     SplashScreen
   ]
