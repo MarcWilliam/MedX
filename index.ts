3fb5c3242e2204878e58cbbce3828970864cb976
DESKTOP-7RJSPLH\mohammed <mohammed.magdy.ali.96@gmail.com>
2018-06-13 15:45:54 +0200

    hot fix

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import encrServece from'./src/helpers/encription-handler';
 import ipfsService from './src/helpers/ipfs-service';
-import { METHODS } from 'http';
 
 class testIpfs{
     encrServece :encrServece;
@@ -12,14 +11,15 @@ class testIpfs{
          this.encrServece = new encrServece();
        
      this.ipfsService.init();
-     let ipfsDataOpject =await this.ipfsService.add('pic.jpg','s');
-     //console.log(ipfsDataOpject);
-    //console.log(await this.ipfsService.retriveIpfs(ipfsDataOpject));
+     let ipfsDataOpject =await this.ipfsService.addIpfs('pic.jpg','s');  // the second parameter is a flag if the input is a file name then flag = 's' , if the input is a data buffer then flag ='r' 
+                                                                         // note that the flag as a defualt = 'r'
+     console.log(ipfsDataOpject);
+    //console.log(await this.ipfsService.retriveIpfs(ipfsDataOpject));  
      
 
     }
 
 }
 
-//var testips = new testIpfs();
-//testips.test();
+var testips = new testIpfs();
+testips.test();
--- src/helpers/ipfs-service.ts
+++ src/helpers/ipfs-service.ts
@@ -14,33 +14,46 @@ export default class IPFSservice {
         this.encrServece = new encrServece();
     }
 //flag  s=> string {file url} , r=>buffer {file data}
-    public async add(data:any,flag:string){
-        if(flag == "r"){
-            return new Promise((resolve , reject)=>{
-            this.addToIpfs(data).then(output=>{
-                resolve(this.encrServece.add(output,'s'));
-           })
-        })
-        }else{
-            return  await this.ReadFile(data);
+    public async addIpfs(data:any,flag:string = "r"){
+        try{
+            if(flag == "r"){
+                return new Promise((resolve , reject)=>{
+                this.addToIpfs(data).then(output=>{
+                    resolve(this.encrServece.add(output,'s'));
+               },err=>{
+                   reject(err);
+               })
+            })
+            }else{
+                return  await this.ReadFile(data);
+            }
+        }catch(err){
+            console.error(err);
         }
+       
 
     }
 
     public async ReadFile(file_Url){
-        return new Promise((resolve , reject)=>{
-            this.SetBuffer(file_Url).then(data=>{
-                this.addToIpfs(data).then(output=>{
-                    //console.log(output);
-                    resolve(this.encrServece.add(output,'s'));
-                    // resolve(output)
+        try{
+            return new Promise((resolve , reject)=>{
+                this.SetBuffer(file_Url).then(data=>{
+                    this.addToIpfs(data).then(output=>{
+                        resolve(this.encrServece.add(output,'s'));
+                    },err=>{
+                        reject(err);
+                    })
                 })
-            })
-        })      
+            })     
+        }catch(err)
+         {
+             console.error(err)
+         }
     }
 
     public async  retriveIpfs(dataOpject : any) {
-        let massage = this.encrServece.retrive(dataOpject);
+        try{
+            let massage = this.encrServece.retrive(dataOpject);
         
         
         if(massage.massage == "success"){
@@ -54,6 +67,10 @@ export default class IPFSservice {
             }else{
                 console.log("Data Opject Not authorized");
             }
+        }catch(err){
+            console.error(err);
+        }
+        
             
         }
         
@@ -61,37 +78,47 @@ export default class IPFSservice {
 
 
     private async addToIpfs(bufferdData) {
-        if (this.ipfs != null) {
-            return new Promise((resolve, reject) => {
-                this.ipfs.files.add(bufferdData, (err, file) => {
-                    if (err) {
-                        reject(err)
-                    }
-                    else {
-                        resolve(file);
-                    }
-
+        try{
+            if (this.ipfs != null) {
+                return new Promise((resolve, reject) => {
+                    this.ipfs.files.add(bufferdData, (err, file) => {
+                        if (err) {
+                            reject(err)
+                        }
+                        else {
+                            resolve(file);
+                        }
+    
+                    });
                 });
-            });
-        } else {
-            console.log("init first");
+            } else {
+                console.log("init first");
+            }
+        }catch(err){
+            console.error(err);
         }
+        
     }
     private async SetBuffer(file_Url) {
-        if(this.ipfs != null){
-        return new Promise((resolve, reject) => {
-            fs.readFile(file_Url, (err, data) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(data);
-                }
-            });
-
-        });
-    }else{
-        console.log("init first")
-    }
+        try{
+            if(this.ipfs != null){
+                return new Promise((resolve, reject) => {
+                    fs.readFile(file_Url, (err, data) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(data);
+                        }
+                    });
+        
+                });
+            }else{
+                console.log("init first")
+            }
+        }catch(err){
+            console.error(err);
+        }
+       
     }
 
 
