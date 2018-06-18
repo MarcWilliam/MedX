2a8c9e17068a8c7bea5cc35be7315fe7c23a5553
marcwafik <marc.wafik@gmail.com>
2018-06-18 21:43:45 +0200

    checkin

--- MedX-developer/src/pages/profile/profile.ts
+++ MedX-developer/src/pages/profile/profile.ts
@@ -24,8 +24,7 @@ export class ProfilePage {
     "email": "john.smith@amail.com",
     "website": "https://www.happy.ae",
     "publicKey": "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
-    "specialization": "Surgery",
-    "PCL" : "Dubai International Hospital"
+    "specialization": "Developer"
 
   };
 
