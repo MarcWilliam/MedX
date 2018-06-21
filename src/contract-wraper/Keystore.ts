d36c597b78d0bb2f5604bc8970b9b06fd844822d
marcwafik <marc.wafik@gmail.com>
2018-06-22 01:13:17 +0200

    added getattrib + add to keystore

--- src/contract-wraper/Keystore.ts
+++ src/contract-wraper/Keystore.ts
@@ -5,10 +5,26 @@ export class Keystore extends Contract {
 
     public get contractName(): string { return "Keystore" };
 
+    public async getAttribs() {
+        try {
+            var contractInstance = await this.getContractInstance();
+
+            var result = {
+                profile: JSON.parse(await contractInstance.profile()),
+            };
+
+            console.log("getAttribs", result);
+            return result;
+        } catch (err) {
+            console.warn(err.message);
+            throw err;
+        }
+    }
+
     public async getKey(record: string | Record) {
         try {
             var contractInstance = await this.getContractInstance();
-            var recAddress = typeof record === 'string'? record : record.address;
+            var recAddress = typeof record === 'string' ? record : record.address;
 
             var result = await contractInstance.rec2key(recAddress);
 
@@ -18,6 +34,13 @@ export class Keystore extends Contract {
             console.warn(err.message);
             throw err;
         }
+    }
+
+    public async add(record: string, encKey: string, extraParams?): Promise<any> {
+        return new Keystore(await this.genericCall("add", {
+            params: [record, encKey],
+            extraParams: extraParams
+        }));
     }
 
 
