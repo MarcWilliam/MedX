c1f88e4bca6d1effb2dadc73c89cfc5f50d9ddf8
marcwafik <marc.wafik@gmail.com>
2018-06-22 15:27:38 +0200

    removed require

--- contracts/keystore.sol
+++ contracts/keystore.sol
@@ -20,7 +20,7 @@ contract Keystore is Ownable {
     event added(Record record, address addedby);
 
     function add(Record _record, string _encKey) public {
-        require(bytes(rec2key[_record]).length == 0); // to protect aggainst enpty keystore attak
+        //require(bytes(rec2key[_record]).length == 0); // to protect aggainst enpty keystore attak
         rec2key[_record] = _encKey;
         emit added(_record, msg.sender);
     }
