278661cec6a0e69eb079c0900ad3ad977f882126
marcwafik <marc.wafik@gmail.com>
2018-08-23 12:58:43 +0200

    updated keystore factory

--- smart-contracts/contracts/keystore.sol
+++ smart-contracts/contracts/keystore.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "./Record.sol";
 
 contract Keystore is Ownable {
 
@@ -15,13 +14,13 @@ contract Keystore is Ownable {
         profile = _profile;
     }
 
-    mapping(address=>string) public rec2key;
+    mapping(uint=>string) public record2key;
 
     event added(Record record, address addedby);
 
     function add(Record _record, string _encKey) public {
-        //require(bytes(rec2key[_record]).length == 0); // to protect aggainst enpty keystore attak
-        rec2key[_record] = _encKey;
+        //require(bytes(record2key[_record]).length == 0); // to protect aggainst enpty keystore attak
+        record2key[_record] = _encKey;
         emit added(_record, tx.origin);
     }
 }
